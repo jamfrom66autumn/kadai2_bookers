2bae2fbe11f11330e0ea4e60dc7d529f90a95dda
EC2 Default User <ec2-user@ip-172-31-2-217.ec2.internal>
2021-12-07 14:13:15 +0000

    initial commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,35 +1,50 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "creating successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @books = Book.all.order(created_at: :desc)
+      # @book=Book.new
+      render :index
+    end
+  end
+
   def index
     @books = Book.all
+    @books = Book.all.order(created_at: :desc)
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def edit
-    @book = Book.find(params)[:id]
-    book.update(book_params)
-    redirect_to book_path(book.id)
-  end
-
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
   end
 
   def update
-    list = List.find(params[:id])
-    list.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path
+    else
+      flash[:alert] = "Updating error."
+      redirect_to book_path
+    end
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed"
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get '/' => 'homes#top'
-  get 'books' => 'books#index'
+  get 'books/new'
+  root to: 'homes#top'
   post 'books' => 'books#create'
+  get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
